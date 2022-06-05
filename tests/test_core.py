5cbcff7c87c240497e45ef0756de36884f59bd0e
Gabriel Gore <gabriel59kg@gmail.com>
2022-06-05 16:45:52 -0400

    demo http_reflect

--- tests/test_core.py
+++ tests/test_core.py
@@ -3,14 +3,21 @@ import pathlib
 import httpx
 import pytest
 
-from csv2http.http import response_details
+from csv2http import http, parser
 
+CWD = pathlib.Path.cwd()
 TESTS_ROOT = pathlib.Path(__file__).parent
 DATA_DIR = TESTS_ROOT / "data"
 
+# relative path strings for all test CSV files
+TEST_CSVS = [str(path.relative_to(CWD)) for path in DATA_DIR.glob("*.csv")]
 
-def test_file_to_wire(http_reflect):
-    response = httpx.post("http://example.com/foobar", json={"foo": "bar"})
-    print(response_details(response, verbose=True))
 
-    assert False
+@pytest.mark.parametrize("filepath", TEST_CSVS)
+def test_file_to_wire(http_reflect, filepath):
+    payload = next(parser.payload_generator(filepath))
+
+    response = httpx.post("http://example.com/foobar", json=payload)
+    print(http.response_details(response, verbose=True))
+
+    assert payload == response.json()
