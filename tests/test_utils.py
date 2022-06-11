15049cf95e364c952ccd79a15bed4d6aaa4d7a92
Gabriel Gore <gabriel59kg@gmail.com>
2022-06-11 07:34:35 -0400

    better summarize_responses test

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,3 +1,5 @@
+import ast
+
 import httpx
 import pytest
 from respx import MockResponse
@@ -19,8 +21,14 @@ from csv2http import utils
 )
 def test_summarize_responses(responses):
     summary = utils.summarize_responses(responses)
-    print(summary)
     assert summary
+
+    _, status_codes = summary.split(" - ")
+    status_codes_dict = ast.literal_eval(status_codes)
+    print(status_codes_dict)
+
+    for r in responses:
+        assert getattr(r, "status_code", r.__class__.__name__) in status_codes_dict
 
 
 if __name__ == "__main__":
